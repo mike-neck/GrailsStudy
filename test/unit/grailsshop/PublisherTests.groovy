031b089b2c8199d5e8d084eaf5d7801f7f3d9653
mike_neck <jkrt3333@gmail.com>
2012-01-02 18:54:02 +0900

    add @Mock Annotation for saving entities

--- test/unit/grailsshop/PublisherTests.groovy
+++ test/unit/grailsshop/PublisherTests.groovy
@@ -9,6 +9,7 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Publisher)
+@Mock([Publisher, Book])
 class PublisherTests {
 
     void testValidation() {
