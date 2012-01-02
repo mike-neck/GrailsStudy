626b34dfe843474859ddacc08aa76637def56564
mike_neck <jkrt3333@gmail.com>
2012-01-02 12:13:53 +0900

    add persistence test for Publisher

--- test/unit/grailsshop/PublisherTests.groovy
+++ test/unit/grailsshop/PublisherTests.groovy
@@ -30,6 +30,25 @@ class PublisherTests {
     void persistence() {
         assert Publisher.count() == 0
         assert Book.getCount() == 0
+
+        def publisher = new Publisher(name: '翔泳社')
+        def book = new Book(
+                title: 'Grails 入門',
+                author: 'mike_neck',
+                price: 2000,
+                releaseDate: createReleaseDate(2012, 1, 2),
+                isbn13: '0123456789321',
+                imageUrl: 'http://localhost:8080/image/img.png',
+                description: 'dummy test book.'
+        )
+        publisher.addToBooks(book).save()
+
+        assert Publisher.count() == 1
     }
 
+    Date createReleaseDate(int year, int month, int date) {
+        def cal = Calendar.getInstance(TimeZone.getTimeZone('Asia/Tokyo'))
+        cal.set(year, month, date)
+        cal.getTime()
+    }
 }
