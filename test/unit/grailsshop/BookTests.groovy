f7e0a6f895555172c8760ca5e848533c130e9cf3
mike_neck <jkrt3333@gmail.com>
2012-01-02 17:36:06 +0900

    add success test case for validation Book

--- test/unit/grailsshop/BookTests.groovy
+++ test/unit/grailsshop/BookTests.groovy
@@ -9,15 +9,18 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Book)
+@Mock([Publisher, Book])
 class BookTests {
 
     def existingBook = {
         return new Book(
+                publisher: new Publisher(name: 'hoge'),
                 title: 'test title',
                 author: 'mike',
                 price: 200,
                 releaseDate: new Date(),
                 isbn13: '1234567890123',
+                imageUrl: 'http://server.com/image/image.jpg',
                 description: 'text'
         )
     }
@@ -157,6 +160,29 @@ class BookTests {
         book = new Book(imageUrl: 'http://server.jp/images/image.png')
         assert book.validate() == false
         assert book.errors[object] == null
+    }
+
+    @Test
+    void successCase() {
+        mockDomain(Publisher)
+        mockForConstraintsTests(Book, [existingBook()])
+
+        def publisher = new Publisher(name: '秀和システム')
+        publisher.save()
+
+        def book = new Book(
+                publisher: publisher,
+                title: 'test title',
+                author: 'mike',
+                price: 100,
+                releaseDate: new Date(),
+                isbn13: '0987654321123',
+                imageUrl: 'http://server.com/images/image.png',
+                description: 'description'
+        )
+        assert book.validate() == true
+
+        publisher.delete()
     }
 
     def createTextToTheSize = {int size ->
