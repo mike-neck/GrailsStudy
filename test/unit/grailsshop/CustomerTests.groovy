3d1d4d42be50f0d98fb99069ccf0636212284879
mike_neck <jkrt3333@gmail.com>
2012-01-07 17:42:14 +0900

    remove unnecessary tests

--- test/unit/grailsshop/CustomerTests.groovy
+++ test/unit/grailsshop/CustomerTests.groovy
@@ -12,6 +12,5 @@ import org.junit.*
 class CustomerTests {
 
     void testSomething() {
-       fail "Implement me"
     }
 }
--- test/unit/grailsshop/WarehouseTests.groovy
+++ test/unit/grailsshop/WarehouseTests.groovy
@@ -12,6 +12,5 @@ import org.junit.*
 class WarehouseTests {
 
     void testSomething() {
-       fail "Implement me"
     }
 }
