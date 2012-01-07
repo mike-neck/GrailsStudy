3e25cfaf5867cfaa85d5199959b0a55a6823561b
mike_neck <jkrt3333@gmail.com>
2012-01-07 16:48:57 +0900

    add test for validation of Account.name

--- grails-app/domain/grailsshop/Account.groovy
+++ grails-app/domain/grailsshop/Account.groovy
@@ -2,6 +2,12 @@ package grailsshop
 
 class Account {
 
+    /**
+     * 名前.
+     */
+    String name
+
     static constraints = {
+        name(blank: false, minSize: 6, matches: /^[a-zA-Z]([a-zA-Z0-9-_]+)/)
     }
 }
