07c080ae328cfb41be6cf11518cfbb471ecba8a2
mike_neck <jkrt3333@gmail.com>
2012-01-09 16:39:57 +0900

    add new field phoneNumber to Warehouse

--- grails-app/domain/grailsshop/Warehouse.groovy
+++ grails-app/domain/grailsshop/Warehouse.groovy
@@ -2,6 +2,18 @@ package grailsshop
 
 class Warehouse {
 
+    /**
+     * 住所.
+     */
+    String address
+
+    /**
+     * 電話番号.
+     */
+    String phoneNumber
+
     static constraints = {
+        address(blank: false)
+        phoneNumber(blank: false, matches: /^[0-9]+[0-9\-]+[0-9]+$/)
     }
 }
--- test/unit/grailsshop/WarehouseTests.groovy
+++ test/unit/grailsshop/WarehouseTests.groovy
@@ -11,6 +11,39 @@ import org.junit.*
 @TestFor(Warehouse)
 class WarehouseTests {
 
-    void testSomething() {
+    @Test
+    void validateAddress() {
+        mockForConstraintsTests(Warehouse)
+        def object = 'address'
+
+        def warehouse = new Warehouse()
+        assert warehouse.validate() == false
+        assert warehouse.errors[object] == 'nullable'
+
+        warehouse = new Warehouse(address: '')
+        assert warehouse.validate() == false
+        assert warehouse.errors[object] == 'blank'
+    }
+
+    @Test
+    void validatePhoneNumber() {
+        mockForConstraintsTests(Warehouse)
+        def object = 'phoneNumber'
+
+        def warehouse = new Warehouse()
+        assert warehouse.validate() == false
+        assert warehouse.errors[object] == 'nullable'
+
+        warehouse = new Warehouse(phoneNumber: '')
+        assert warehouse.validate() == false
+        assert warehouse.errors[object] == 'blank'
+
+        warehouse = new Warehouse(phoneNumber: 'aaa-bbb')
+        assert warehouse.validate() == false
+        assert warehouse.errors[object] == 'matches'
+
+        warehouse = new Warehouse(phoneNumber: '00-00')
+        warehouse.validate()
+        assert warehouse.errors[object] == null
     }
 }
